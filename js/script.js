0fc1c43f4e0471d5db8ecf3e3be7bee63cb3b511
s-nik <nickolay.soshin@gmail.com>
2019-05-28 15:54:38 +0400

    change sliders dots, fix some responsive bugs

--- js/script.js
+++ js/script.js
@@ -1,92 +1,18 @@
-    $('.responsive').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        arrows: true,
-        responsive: [
-            {
-                breakpoint: 1350,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 778,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 415,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
-
-    // $('.responsive-2').slick({
-    //     dots: false,
-    //     infinite: true,
-    //     speed: 500,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 1,
-    //     arrows: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1200,
-    //             settings: {
-    //                 slidesToShow: 3,
-    //                 slidesToScroll: 1,
-    //                 infinite: true,
-    //                 dots: false
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 991,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 slidesToScroll: 1
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //             }
-    //         }
-    //     ]
-    // });
-
     $('.owl-carousel-films').owlCarousel({
         loop:true,
         margin:10,
         responsiveClass:true,
+        dots: false,
         responsive:{
             0:{
                 items:2,
                 nav:true
             },
-            768:{
-                items:2,
+            550:{
+                items:3,
                 nav:true
             },
-            1000:{
+            768:{
                 items:4,
                 nav:true,
                 loop:true
@@ -98,6 +24,7 @@
         loop:true,
         margin:10,
         responsiveClass:true,
+        dots: false,
         responsive:{
             0:{
                 items:1,
