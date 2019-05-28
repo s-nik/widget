a553f65bce78cc9061e7164951bd9fde975943d7
s-nik <nickolay.soshin@gmail.com>
2019-05-28 12:53:02 +0400

    change sliders

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@
                 }
             },
             {
-                breakpoint: 680,
+                breakpoint: 778,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1
@@ -39,36 +39,88 @@
         ]
     });
 
-    $('.responsive-2').slick({
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: true,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: false
-                }
+    // $('.responsive-2').slick({
+    //     dots: false,
+    //     infinite: true,
+    //     speed: 500,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     arrows: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 1200,
+    //             settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 1,
+    //                 infinite: true,
+    //                 dots: false
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 991,
+    //             settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //             }
+    //         }
+    //     ]
+    // });
+
+    $('.owl-carousel-films').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2,
+                nav:true
             },
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
+            768:{
+                items:2,
+                nav:true
             },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+            1000:{
+                items:4,
+                nav:true,
+                loop:true
             }
-        ]
-    });
+        }
+    })
+
+    $('.owl-carousel-channels').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            380:{
+                items:2,
+                margin:15,
+                nav:true
+            },
+                768:{
+                    items:3,
+                    margin:15,
+                    nav:true
+                },
+                    991:{
+                        items:5,
+                        nav:true
+                    },
+                    1200:{
+                        items:6,
+                        nav:true,
+                        loop:true
+                    }
+        }
+    })
